ccea161772ef503ddd23dcc5a4faf9176e6d9df0
Tushar Agrawal <Tushar Agrawal>
2016-01-04 21:39:01 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,237 @@
+
+
+<script>
+  (function() {
+    var cx = '006668939710513893571:gc-ccns-b48';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dorker - A new way to Google</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <!-- Custom Fonts -->
+
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/creative.css" type="text/css">
+
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Dorker</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Services</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">Strengths</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+	
+	
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h3> <b>A new way to Google.</b></h3>
+			
+<div align="center">
+<form action="red.php" method="POST"><font color="white">
+
+<input type="text" name="name" class="form-control" style="width:350px;" placeholder="Enter words to search" x-webkit-speech ></font>
+<br>
+<select name="type" class="form-control" style="width:350px;"  >
+<option value="None" selected>Select any Category</option>
+<option value="None">General Search</option>
+<option value="txt" >Text Files</option>
+<option value="ppt" >Presentations</option>
+<option value="xls" >Spreadsheets</option>
+<option value="pdf">PDF</option>
+<option value="mp4" >Movies</option>
+<option value="jpg">Images</option>
+ </select>
+<br><br><br>
+
+              
+               <input type="submit" value="Search"  class="btn btn-primary btn-xl page-scroll"  >
+			   </form>
+</div>
+
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">We've got what you need!</h2>
+                    <hr class="light">
+                    <p class="text-faded">AW Dorker has been developed by <br><b>Tushar Agrawal(tushalien) and Abhishek Chaudhary(worm4047).</b>
+					
+<br>It works on Google Dorking.
+	Google Dorking is a term that refers to the practice of applying advanced search techniques 
+	and specialized search engine parameters to discover confidential information from companies and individuals
+	that wouldn't typically show up during a normal web search.
+AW Dorker uses Google Dorking to drastically reduce search results to only those that are required by users.</p>
+                    <a href="#" class="btn btn-default btn-xl">Get Started!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">At Your Service</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-diamond wow bounceIn text-primary" data-wow-delay=".2s"></i>
+                        <h3>Clean Design</h3>
+                        <p class="text-muted">Our clean designs make the site easy to access.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".4s"></i>
+                        <h3>Source Code Available</h3>
+                        <p class="text-muted">You can use modify the source code as it is available on Github.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".6s"></i>
+                        <h3>Up to Date</h3>
+                        <p class="text-muted">We update dependencies to keep things fresh.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-heart wow bounceIn text-primary" data-wow-delay=".8s"></i>
+                        <h3>Very Useful</h3>
+                        <p class="text-muted">We make our web apps which are beneficial to you.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+		
+    </section>
+
+    <section class="no-padding" id="portfolio">
+       
+    </section>
+
+   
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Let's Get In Touch!</h2>
+                    <hr class="primary">
+                    <p>Got a suggestion or a request. That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                </div></div><div class="row">
+                <div class="col-lg-4 col-lg-offset-2 text-center">
+                    <i class="fa fa-phone fa-3x wow bounceIn"  data-wow-delay=".8s"></i>
+                    <p>+91-74082-84972</p>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".8s"></i>
+                    <p><a href="mailto:a.com">abhidagar@gmail.com</a></p>
+                </div>
+				  </div>
+            </div>
+        </div> <?php $handle = fopen("counter.txt", "r");  
+ $counter = (int ) fread($handle,20); fclose ($handle); $counter++; 
+ echo' <div align="right" style="padding-right:20px;" ><strong> Site Hits:'. $counter . ' </strong></div> ' ; 
+ $handle = fopen("counter.txt", "w" ); fwrite($handle,$counter) ; fclose ($handle) ;  ?>
+    </section>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script src="js/wow.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/creative.js"></script>
+
+</body>
+
+</html>
